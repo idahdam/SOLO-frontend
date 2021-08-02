efa86692e6736723b64e6db051a106c06ce71623
Hadi <nasshitell@gmail.com>
2021-08-02 16:09:47 +0700

    70^

--- src/pages/ArtistId/index.js
+++ src/pages/ArtistId/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./index.css";
 import niki from "../../assets/artist/niki.png";
+import lowkey from "../../assets/genre/lowkey.png";
+import { Link } from "react-router-dom";
 
 const ArtistId = () => {
   return (
@@ -22,6 +24,43 @@ const ArtistId = () => {
               <div className="artistid-hero-description">
                 Here are list of songs by NIKI. Click the song to see its
                 reviews!
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="artistid-songlist-container">
+          <div className="artistid-songlist-title">Song List</div>
+          <div className="artistid-songlist-row">
+            <div className="artistid-songlist-column">
+              <div className="artistid-songlist-each-row">
+                <div className="artistid-songlist-each-column artistid-songlist-each-left">
+                  <Link to="/song/id">
+                    <img
+                      src={lowkey}
+                      alt="song"
+                      className="artistid-songlist-image"
+                    />
+                  </Link>
+                </div>
+                <div className="artistid-songlist-each-column artistid-songlist-each-right">
+                  <div className="artistid-songlist-text">Lowkey</div>
+                </div>
+              </div>
+            </div>
+            <div className="artistid-songlist-column">
+              <div className="artistid-songlist-each-row">
+                <div className="artistid-songlist-each-column artistid-songlist-each-left">
+                  <Link to="/song/id">
+                    <img
+                      src={lowkey}
+                      alt="song"
+                      className="artistid-songlist-image"
+                    />
+                  </Link>
+                </div>
+                <div className="artistid-songlist-each-column artistid-songlist-each-right">
+                  <div className="artistid-songlist-text">Lowkey</div>
+                </div>
               </div>
             </div>
           </div>
--- src/pages/SongId/index.js
+++ src/pages/SongId/index.js
@@ -1,7 +1,98 @@
 import React from "react";
+import "./index.css";
+import lowkey from "../../assets/genre/lowkey.png";
 
 const SongId = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="songid-container">
+        <div className="songid-hero">
+          <div className="songid-hero-row">
+            <div className="songid-hero-column songid-hero-left">
+              <div className="songid-hero-image-container">
+                <img src={lowkey} alt="songId" className="songid-hero-image" />
+              </div>
+            </div>
+            <div className="songid-hero-column songid-hero-right">
+              <div className="songid-hero-text">Lowkey</div>
+              <div className="songid-hero-description">
+                Here is a song by NIKI. Scroll to see its reviews!
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="songid-reviews">
+          <div className="songid-reviews-title">Reviews</div>
+          <div className="songid-reviews-container">
+            <div className="songid-reviews-row">
+              <div className="songid-reviews-column">
+                <div className="songid-reviews-content-container">
+                  <div className="songid-reviews-content-review">
+                    “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque et augue sit amet augue dignissim imperdiet. Ut
+                    vel elementum mauris. Etiam nec elementum odio, ac malesuada
+                    felis. Etiam euismod a risus imperdiet eleifend. Fusce
+                    accumsan tellus rhoncus, rhoncus massa non, dapibus risus.
+                    Cras facilisis vitae sem vel consequat. Maecenas semper
+                    lobortis arcu, at semper libero ultricies sed. Nulla maximus
+                    pulvinar orci ut varius. Integer placerat consectetur magna
+                    sed commodo. “
+                  </div>
+                  <div className="songid-reviews-content-reviewer">
+                    Muhammad Hadi - 4.5
+                  </div>
+                </div>
+              </div>
+              <div className="songid-reviews-column">
+                <div className="songid-reviews-content-container">
+                  <div className="songid-reviews-content-review">
+                    “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque et augue sit amet augue dignissim imperdiet. Ut
+                    vel elementum mauris. Etiam nec elementum odio, ac malesuada
+                    felis. Etiam euismod a risus imperdiet eleifend. “
+                  </div>
+                  <div className="songid-reviews-content-reviewer">
+                    Muhammad Hadi - 4.5
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="songid-reviews-row">
+              <div className="songid-reviews-column">
+                <div className="songid-reviews-content-container">
+                  <div className="songid-reviews-content-review">
+                    “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque et augue sit amet augue dignissim imperdiet. Ut
+                    vel elementum mauris. Etiam nec elementum odio, ac malesuada
+                    felis. Etiam euismod a risus imperdiet eleifend. “
+                  </div>
+                  <div className="songid-reviews-content-reviewer">
+                    Muhammad Hadi - 4.5
+                  </div>
+                </div>
+              </div>
+              <div className="songid-reviews-column">
+                <div className="songid-reviews-content-container">
+                  <div className="songid-reviews-content-review">
+                    “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque et augue sit amet augue dignissim imperdiet. Ut
+                    vel elementum mauris. Etiam nec elementum odio, ac malesuada
+                    felis. Etiam euismod a risus imperdiet eleifend. Fusce
+                    accumsan tellus rhoncus, rhoncus massa non, dapibus risus.
+                    Cras facilisis vitae sem vel consequat. Maecenas semper
+                    lobortis arcu, at semper libero ultricies sed. “
+                  </div>
+                  <div className="songid-reviews-content-reviewer">
+                    Muhammad Hadi - 4.5
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SongId;

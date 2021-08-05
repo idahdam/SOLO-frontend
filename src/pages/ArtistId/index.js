d5926b893d53358715f94e68e071dcc502b711e3
Hadi <nasshitell@gmail.com>
2021-08-05 17:40:47 +0700

    belom dibug

--- src/pages/ArtistId/index.js
+++ src/pages/ArtistId/index.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Swal from "sweetalert2";
 import "./index.css";
 import { Link, useParams } from "react-router-dom";
 import { artistService } from "../../services/artistService";
+import { adminService } from "../../services/adminService";
 
 const adminEmail = process.env.REACT_APP_ADMIN_EMAIL;
 const ArtistId = () => {
   const { isAuthenticated, user } = useAuth0();
   const [artistId, setArtistId] = useState([]);
   const [artistSongs, setArtistSongs] = useState([]);
-  const { id } = useParams();
+  const { name, id } = useParams();
   useEffect(() => {
     const fetchArtistByidWithSongs = async (id) => {
       const response = await artistService.getAllArtistWithSongs(id);
@@ -27,6 +29,33 @@ const ArtistId = () => {
     fectchArtistById(id);
     fetchArtistByidWithSongs(id);
   }, [id]);
+
+  const handleDeleteArtist = () => {
+    Swal.fire({
+      title: `About to delete artist. Are you sure?`,
+      showDenyButton: true,
+      confirmButtonText: `Yes`,
+      showCancelButton: `Yes`,
+      denyButtonText: `No`,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        onDeleteArtist(id);
+      } else if (result.isDenied) {
+        Swal.fire("Operation canceled.", "", "info");
+      }
+    });
+  };
+  const onDeleteArtist = async (id) => {
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const response = await adminService
+        .deleteArtistById(id)
+        .then(Swal.fire(`Artist is deleted.`))
+        .then((window.location.href = "/"));
+    } catch (error) {
+      Swal.fire("error " + error);
+    }
+  };
 
   if (artistId.length === 0) return null;
   return (
@@ -54,9 +83,13 @@ const ArtistId = () => {
               {isAuthenticated && user.email === adminEmail ? (
                 <>
                   <button type="button" className="admin-only-button">
-                    Edit
+                    <Link to={`/artist/${id}-${name}/edit`}>Edit</Link>
                   </button>{" "}
-                  <button type="button" className="admin-only-button">
+                  <button
+                    type="button"
+                    className="admin-only-button"
+                    onClick={() => handleDeleteArtist()}
+                  >
                     Delete
                   </button>
                 </>
--- src/pages/Edit/EditArtist/index.js
+++ src/pages/Edit/EditArtist/index.js
@@ -1,15 +1,119 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import axios from "axios";
+import { adminService } from "../../../services/adminService";
+import { artistService } from "../../../services/artistService";
 
-const { REACT_APP_ADMIN_EMAIL } = process.env;
+const { REACT_APP_PRESET_NAME, REACT_APP_CLOUD_URL, REACT_APP_ADMIN_EMAIL } =
+  process.env;
+const url = REACT_APP_CLOUD_URL;
+const preset = REACT_APP_PRESET_NAME;
 const adminEmail = REACT_APP_ADMIN_EMAIL;
 const EditArtist = () => {
+  const { id } = useParams();
+  const [artistName, setArtistName] = useState();
+  const [image, setImage] = useState(null);
+  const [artist, setArtist] = useState([]);
+  const [imageLink, setImageLink] = useState();
   const { isAuthenticated, user } = useAuth0();
+  const onSelectFile = (e) => {
+    // I've kept this example simple by using the first image instead of multiple
+    setImage(e.target.files[0]);
+  };
+
+  const handleAddArtist = () => {
+    Swal.fire({
+      title: `About to update a song. Are you sure?`,
+      showDenyButton: true,
+      confirmButtonText: `Yes`,
+      showCancelButton: `Yes`,
+      denyButtonText: `No`,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        onSubmitEdit();
+      } else if (result.isDenied) {
+        Swal.fire("Operation canceled.", "", "info");
+      }
+    });
+  };
+
+  const onSubmitEdit = async () => {
+    if (image !== null) {
+      const formData = new FormData();
+      formData.append("file", image);
+      formData.append("upload_preset", preset);
+      try {
+        const photoUpload = await axios.post(url, formData);
+        const imageUrl = await photoUpload.data.secure_url;
+        // eslint-disable-next-line no-unused-vars
+        const editArtist = await adminService
+          .updateArtistById(id, {
+            picture: imageUrl,
+            name: artistName,
+          })
+          .then(Swal.fire("Artist updated"));
+      } catch (error) {}
+    } else {
+      try {
+        try {
+          // eslint-disable-next-line no-unused-vars
+          const editArtist = await adminService
+            .updateArtistById(id, {
+              picture: imageLink,
+              name: artistName,
+            })
+            .then(Swal.fire("Artist updated"));
+        } catch (error) {
+          Swal.fire("error " + error);
+        }
+      } catch (error) {
+        Swal.fire("error " + error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchArtist = async () => {
+      const response = await artistService.getArtistById(id);
+      console.log(response.data);
+      setArtist(response.data);
+      setImageLink(response.data[0].artist_picture);
+    };
+
+    fetchArtist(id);
+  }, [id]);
+
+  if (artist.length === 0) return null;
   return (
     <>
       {isAuthenticated && user.email === adminEmail ? (
-        <div className="edit-container">ARTIST PAGE</div>
+        <div className="edit-container">
+          <div className="add-container-title">Edit Artist</div>
+          <div className="add-artist">
+            <label className="add-label">Artist Name</label>
+            <br />
+            <input
+              type="text"
+              className="add-input"
+              onChange={(e) => setArtistName(e.target.value)}
+              defaultValue={artist[0].artist_name}
+            />
+            <br />
+            <label className="add-label">Artist Picture</label>
+            <br />
+            <input type="file" className="add-input" onChange={onSelectFile} />
+            <br />
+            <button
+              type="button"
+              className="submit-add"
+              onClick={() => handleAddArtist()}
+            >
+              Edit Artist
+            </button>
+          </div>
+        </div>
       ) : (
         <>
           <Redirect to="/" />
